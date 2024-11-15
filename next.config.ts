733b8b468cef868d4ec07d5cdc29b286a26d5fdc
JamesHKCampbell <jameskeenancampbell@gmail.com>
2024-11-15 09:08:52 -0500

    0.11

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/2024-NFL-strength-model-forecast',
-  assetPrefix: '/2024-NFL-strength-model-forecast',
 };
 
 export default nextConfig;
