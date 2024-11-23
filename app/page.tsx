a84a64060a5789bd24658f4f03c501f51cb3fa60
JamesHKCampbell <jameskeenancampbell@gmail.com>
2024-11-22 19:15:43 -0500

    0.21

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         This modeling project will calculate weekly-updating Team Strength scores for teams in the NFL by analyzing not just how many games each team has won/lost but which opponents teams have won/lost to. 
       </p>
       <p className="text-medium font-light mb-2 font-sans">
-        This project uses a <b>"maximum likelihood"</b> model to assign a strength score between 1 and 100 to every team, adjusting the scores to fit best with the actual records of head-to-head wins and losses. Beating a great team or losing to a bad one will shift these scores the most, while an "expected" result moves the needle less. 
+        This project uses a <b>`&quot;`maximum likelihood`&quot;`</b> model to assign a strength score between 1 and 100 to every team, adjusting the scores to fit best with the actual records of head-to-head wins and losses. Beating a great team or losing to a bad one will shift these scores the most, while an `&quot;`expected`&quot;` result moves the needle less. 
       </p>
       <p className="text-medium font-light mb-4 font-sans">
         With strength of schedule varying significantly between teams - especially partway through seasons - this Team Strength model can give a significantly more nuanced measure of prowess than simple Win-Loss records.
