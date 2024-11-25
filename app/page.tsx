fc926ef9fc8d6d2eaf55644686337b7e75cbe0f4
JamesHKCampbell <jameskeenancampbell@gmail.com>
2024-11-24 19:04:52 -0500

    0.50

--- app/page.tsx
+++ app/page.tsx
@@ -83,13 +83,15 @@ export default function Home() {
 
       {/* Description paragraphs */}
       <p className="text-sm mb-2 font-sans">
-        This modeling project will calculate weekly-updating Team Strength scores for teams in the NFL by analyzing not just how many games each team has won/lost but which opponents teams have won/lost to. 
+        This modeling project calculates weekly-updating Team Strength scores for teams in the NFL by analyzing not just how many games each team has won/lost but which opponents teams have won/lost to. 
       </p>
       <p className="text-sm mb-2 font-sans">
-        This project uses a <b>&quot;maximum likelihood&quot;</b> model to assign a strength score between 1 and 100 to every team, adjusting the scores to fit best with the actual records of head-to-head wins and losses. Beating a great team or losing to a bad one will shift these scores the most, while an &quot;expected&quot; result moves the needle less. 
+        This project uses a <b>&quot;maximum likelihood&quot;</b> model to assign a strength score between 1 and 100 to every team, adjusting the scores to fit best with the actual records of head-to-head wins and losses. Beating a great team or losing to a bad one will shift these scores the most, while an &quot;expected&quot; result moves the needle less.         With strength of schedule varying significantly between teams - especially partway through seasons - this Team Strength model can give a significantly more nuanced measure of prowess than simple Win-Loss records.
+      </p>
+      <p className="text-sm mb-2 font-sans">
       </p>
       <p className="text-sm mb-8 font-sans">
-        With strength of schedule varying significantly between teams - especially partway through seasons - this Team Strength model can give a significantly more nuanced measure of prowess than simple Win-Loss records.
+        The probabilistic outcomes - the chance to make the playoffs, win the division, or win the Super Bowl - are based on 10,000 simulations of the remaining season and resulting playoffs brackets.
       </p>
       <h2 className="text-sm mb-2 font-sans"><b>Model last updated:</b> {page_metadata.updated_at}</h2>
 
@@ -98,7 +100,7 @@ export default function Home() {
 
       {/* Footer */}
       <p className="text-sm font-light mb-4 font-sans"></p>
-      <p className="text-sm font-sans">Built by James H. K. Campbell for a bit of fun</p>
+      <p className="text-sm font-sans">Built by James H. K. Campbell, for a bit of fun</p>
       <a className="text-sm text-blue-600 hover:text-blue-800 underline  font-sans" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jameshkc">LinkedIn</a>
       <p className="text-sm font-sans mb-10"></p>
       <a className="text-sm font-bold hover:text-blue-800 mb-2 font-sans" target="_blank" rel="noopener noreferrer" href="https://www.mainspringlab.com">Back to: Mainspring Lab Home Page</a>
