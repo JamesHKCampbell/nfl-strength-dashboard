4ba75249882169ed9115317ec24c73e02421216a
JamesHKCampbell <jameskeenancampbell@gmail.com>
2024-12-24 11:38:35 -0500

    0.66

--- app/components/UpcomingGames.tsx
+++ app/components/UpcomingGames.tsx
@@ -7,6 +7,7 @@ interface GameForecast {
   Visitor: string;
   Home_Win_Likelihood: number;
   Visitor_Win_Likelihood: number;
+  Game_Excitement_Score: number;
 }
 
 const UpcomingGames = () => {
@@ -22,7 +23,8 @@ const UpcomingGames = () => {
           Home: data.Home[index],
           Visitor: data.Visitor[index],
           Home_Win_Likelihood: data.Home_Win_Likelihood[index],
-          Visitor_Win_Likelihood: data.Visitor_Win_Likelihood[index]
+          Visitor_Win_Likelihood: data.Visitor_Win_Likelihood[index],
+          Game_Excitement_Score: data.Game_Excitement_Score[index]
         }));
         setGames(gamesArray);
       })
@@ -40,6 +42,13 @@ const UpcomingGames = () => {
     // Using similar colors to your existing table
     if (isDarkMode) return `rgba(220, 139, 24, ${opacity})`;
     return `rgba(236, 174, 87, ${opacity})`;  
+    };
+
+  const getExcitementColor = (score: number) => {
+    const opacity = score / 100;
+    // Using similar colors to your existing table
+    if (isDarkMode) return `rgba(220, 139, 24, ${opacity})`;
+    return `rgba(171, 219, 118, ${opacity})`;  
     };
 
   const formatDate = (dateString: string) => {
@@ -62,6 +71,7 @@ const UpcomingGames = () => {
             <th className="p-2 text-center">@</th>
             <th className="p-2 text-left">Home Team</th>
             <th className="p-2 text-left">Win Probability</th>
+            <th className="p-2 text-left">Game Excitement Score</th>
           </tr>
         </thead>
         <tbody>
@@ -79,13 +89,20 @@ const UpcomingGames = () => {
               </td>
               <td className="p-2 text-sm text-center">@</td>
               <td className="p-2 text-sm">{game.Home}</td>
-              <td className="p-2 text-sm" 
+              <td className="p-2 text-sm border-r-2 border-r-black dark:border-r-white" 
                   style={{ 
                     backgroundColor: getProbabilityColor(game.Home_Win_Likelihood),
                     // color: game.Home_Win_Likelihood > 75 ? 'white' : 'black'
                     color: isDarkMode ? 'white' : 'black'
                   }}>
                 {game.Home_Win_Likelihood}%
+              </td>
+              <td className="p-2 text-sm text-center w-60" 
+                  style={{ 
+                    backgroundColor: getExcitementColor(game.Game_Excitement_Score),
+                    color: isDarkMode ? 'white' : 'black'
+                  }}>
+                {game.Game_Excitement_Score}
               </td>
             </tr>
           ))}
